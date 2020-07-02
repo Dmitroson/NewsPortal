8ed3f809ac2527c1266769a29e9e9b3ffd1d9b49
Sasha <sanyokkotenko2002@gmail.com>
2020-07-02 14:32:16 +0300

    deploy to azure, add connectionString for azure

--- NewsPortal/Models/NHibernate/NHibernateHelper.cs
+++ NewsPortal/Models/NHibernate/NHibernateHelper.cs
@@ -11,6 +11,7 @@ public class NHibernateHelper
         ISessionFactory sessionFactory = Fluently.Configure()
             .Database(MsSqlConfiguration.MsSql2012
                 .ConnectionString(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=NewsPortalDb;Integrated Security=True")
+                //.ConnectionString(@"Data Source=testnewsportal20200702113407dbserver.database.windows.net;Initial Catalog=NewsPortal20200702133310_db;User ID=Dmitroson;Password=#include_Root;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
                 .ShowSql()
             )
             .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Article>())
