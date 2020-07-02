339e1fbe484679dafd493c662db54bb01035ff77
Sasha <sanyokkotenko2002@gmail.com>
2020-07-02 13:31:04 +0300

    add article controller with Index method

---
+++ NewsPortal/Controllers/ArticleController.cs
@@ -0,0 +1,20 @@
+﻿using NewsPortal.Models;
+using NHibernate;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace NewsPortal.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                var articles = session.Query<Article>().ToList();
+                return View(articles);
+            }
+        }
+    }
+}
