9bf90b19aa55f041053fc8df6ab0062b5e5e34e1
Sasha <sanyokkotenko2002@gmail.com>
2020-07-15 01:21:15 +0300

    fix after crash

--- NewsPortal/Controllers/AdminController.cs
+++ NewsPortal/Controllers/AdminController.cs
@@ -1,10 +1,7 @@
 ﻿using NewsPortal.Models;
 using NHibernate;
-using NHibernate.Mapping;
-using NHibernate.Util;
 using System;
 using System.Collections.Generic;
-using System.Deployment.Internal;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -16,33 +13,24 @@ namespace NewsPortal.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index(string sortOrder = "Date", int page = 1, string keywords = "", string filterString = "")
+        public ActionResult Index(string sortOrder = "Date", int page = 1, string parameters = "")
         {
             using (ISession session = NHibernateHelper.OpenSession())
             {
+                ParseParams(parameters, out string searchString, out string filterString);
+
                 var articles = session.Query<Article>();
 
                 DateFilter filter = new DateFilter(filterString);
                 articles = filter.FilterByDate(articles);
 
-                if (keywords != "")
+                if (searchString != "")
                 {
-                    articles = articles.Where(a => a.Title.Contains(keywords)
-                                                || a.Description.Contains(keywords));
+                    articles = articles.Where(a => a.Title.Contains(searchString)
+                                                || a.Description.Contains(searchString));
                 }
 
-                switch (sortOrder)
-                {
-                    case "Title":
-                        articles = articles.OrderBy(a => a.Title);
-                        break;
-                    case "Description":
-                        articles = articles.OrderBy(a => a.Description);
-                        break;
-                    default:
-                        articles = articles.OrderByDescending(a => a.PubDate);
-                        break;
-                }
+                articles = Sort(articles, sortOrder);
 
                 var articlesList = articles.ToList();
                 int pageSize = 10;
@@ -230,8 +218,7 @@ namespace NewsPortal.Controllers
                         article.Comments.Add(comment);
                         session.Save(article);
                         transaction.Commit();
-                        Response.Redirect(Request.RawUrl);
-                        //return View("Details", article);                        
+                        Response.Redirect(Request.RawUrl);                      
                     }
                 }
             }
@@ -251,6 +238,54 @@ namespace NewsPortal.Controllers
                     transaction.Commit();
                     return RedirectToRoute(new { controller = "Admin", action = "Details", id = articleId.ToString() });
                 }
+            }
+        }
+
+        private IQueryable<Article> Sort(IQueryable<Article> articles, string order)
+        {
+            switch (order)
+            {
+                case "Title":
+                    articles = articles.OrderBy(a => a.Title);
+                    break;
+                case "Description":
+                    articles = articles.OrderBy(a => a.Description);
+                    break;
+                default:
+                    articles = articles.OrderByDescending(a => a.PubDate);
+                    break;
+            }
+            return articles;
+        }
+
+        private void ParseParams(string paramsString, out string searchString, out string filterString)
+        {
+            searchString = "";
+            filterString = "";
+            string[] paramsArray = paramsString.Split('&');
+
+            switch (paramsArray.Length)
+            {
+                case 1:
+                    int foundIndex = paramsArray[0].IndexOf("=");
+                    if (paramsArray[0].Contains("searchString"))
+                    {
+                        searchString = paramsArray[0].Substring(foundIndex + 1);
+                        filterString = "";
+                    }
+                    else
+                    {
+                        filterString = paramsArray[0].Substring(foundIndex + 1);
+                        searchString = "";
+                    }
+                    break;
+                case 2:
+                    int foundIndex1 = paramsArray[0].IndexOf("=");
+                    searchString = paramsArray[0].Substring(foundIndex1 + 1);
+
+                    int foundIndex2 = paramsArray[1].IndexOf("=");
+                    filterString = paramsArray[1].Substring(foundIndex2 + 1);
+                    break;
             }
         }
     }
---
+++ NewsPortal/Scripts/Handlers/ParamsHandlers.js
@@ -0,0 +1,104 @@
+﻿let searchButton = document.getElementById('searchButton');
+
+searchButton.addEventListener('click', function () {
+    saveCheckboxesToSessionData();
+
+    let searchString = document.getElementById('searchString');
+    sessionStorage.setItem('searchString', searchString.value);
+    let params = document.getElementById('params');
+    params.value = loadSessionData();
+});
+
+let filter = document.getElementById('filter');
+
+filter.addEventListener('click', function () {
+    saveCheckboxesToSessionData();
+    let params = document.getElementById('params');
+    params.value = loadSessionData();
+});
+
+
+function saveCheckboxesToSessionData() {
+    let checkboxToday = document.getElementById('today');
+    let checkboxYesterday = document.getElementById('yesterday');
+    let checkboxWeek = document.getElementById('week');
+    let checkboxAll = document.getElementById('all');
+
+    if (checkboxToday.checked) {
+        sessionStorage.setItem('today', '1');
+    } else {
+        sessionStorage.setItem('today', '0');
+    }
+
+    if (checkboxYesterday.checked) {
+        sessionStorage.setItem('yesterday', '1');
+    } else {
+        sessionStorage.setItem('yesterday', '0');
+    }
+
+    if (checkboxWeek.checked) {
+        sessionStorage.setItem('week', '1');
+    } else {
+        sessionStorage.setItem('week', '0');
+    }
+
+    if (checkboxAll.checked) {
+        sessionStorage.setItem('all', '1');
+    } else {
+        sessionStorage.setItem('all', '0');
+    }
+}
+
+function saveSorting() {
+    let dateOrder = document.getElementById('date');
+    let titleOrder = document.getElementById('title');
+    let descriptionOrder = document.getElementById('description');
+
+    dateOrder.addEventListener('click', function () {
+        sessionStorage.setItem('sortOrder', 'date');
+    });
+
+    titleOrder.addEventListener('click', function () {
+        sessionStorage.setItem('sortOrder', 'title');
+    });
+
+    descriptionOrder.addEventListener('click', function () {
+        sessionStorage.setItem('sortOrder', 'description');
+    });
+}
+
+function loadSessionData() {
+    let params = '';
+
+    let searchString = document.getElementById('searchString').value;
+    if (searchString != '')
+        params += 'searchString=' + searchString;
+
+    let today = sessionStorage.getItem('today');
+    let yesterday = sessionStorage.getItem('yesterday');
+    let week = sessionStorage.getItem('week');
+    let all = sessionStorage.getItem('all');
+    let filterString = '';
+
+    if (today == '1')
+        filterString += 'today ';
+
+    if (yesterday == '1')
+        filterString += 'yesterday ';
+
+    if (week == '1')
+        filterString += 'week ';
+
+    if (all == '1')
+        filterString += 'all ';
+
+    if (filterString != '') {
+        if (params != '') {
+            params += '&filterString=' + filterString;
+        } else {
+            params += 'filterString=' + filterString;
+        }
+    }
+
+    return params;
+}
