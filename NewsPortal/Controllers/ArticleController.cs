a5f594a80c8309289148fa4e607899b77f676997
Sasha <sanyokkotenko2002@gmail.com>
2020-07-15 01:27:42 +0300

    clear code

--- NewsPortal/Controllers/ArticleController.cs
+++ NewsPortal/Controllers/ArticleController.cs
@@ -18,8 +18,6 @@ namespace NewsPortal.Controllers
 
                 var articles = session.Query<Article>().Where(a => a.PubDate <= DateTime.Now && a.Visibility == true);
 
-                ParseParams(parameters, out string searchString, out string filterString);
-
                 DateFilter filter = new DateFilter(filterString);
                 articles = filter.FilterByDate(articles);
 
