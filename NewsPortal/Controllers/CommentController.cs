b33d8a06029ec7a54914a82d6c23e32e501aa571
Sasha <sanyokkotenko2002@gmail.com>
2020-07-22 19:18:35 +0300

    move commenting functionality to CommentController

---
+++ NewsPortal/Controllers/CommentController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Business.Models;
+using Business.Services;
+using NewsPortal.ViewModels;
+using NHibernate.DAL.Repositories;
+using System.Web.Mvc;
+
+namespace NewsPortal.Controllers
+{
+    public class CommentController : Controller
+    {
+        private Service service;
+
+        public CommentController()
+        {
+            service = new Service(new UnitOfWork());
+        }
+
+        // GET: Comment
+        public ActionResult CommentsList(int articleId)
+        {
+            var comments = service.GetComments(articleId);
+            return PartialView("CommentsList", comments);
+        }
+
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CommentViewModel commentViewModel, int articleId)
+        {
+            if (ModelState.IsValid)
+            {
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<CommentViewModel, Comment>());
+                var mapper = new Mapper(config);
+                var comment = mapper.Map<Comment>(commentViewModel);
+                service.CreateComment(comment, articleId);
+                Response.Redirect(Request.RawUrl);
+            }
+            return View(commentViewModel);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int id)
+        {
+            var articleId = service.GetArticleIdByCommentId(id);
+            service.DeleteComment(id);
+            return RedirectToRoute(new { controller = "Admin", action = "Details", id = articleId.ToString() });
+        }
+    }
+}
