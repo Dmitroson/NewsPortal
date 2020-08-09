2d6726241497de59f094673fb908fd0ea0274c05
Sasha <sanyokkotenko2002@gmail.com>
2020-08-10 02:47:44 +0300

    updata gitignore

--- NewsPortal/Controllers/CommentController.cs
+++ NewsPortal/Controllers/CommentController.cs
@@ -2,10 +2,6 @@
 using Business.Services;
 using NewsPortal.Attributes;
 using NewsPortal.ViewModels;
-using NHibernate.DAL.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Web;
 using System.Web.Mvc;
 
 namespace NewsPortal.Controllers
@@ -51,7 +47,6 @@ namespace NewsPortal.Controllers
 
         public ActionResult SureDelete(Comment comment)
         {
-            ChangeLanguage();
             if (comment != null)
                 return PartialView("Delete", comment);
             return HttpNotFound();
@@ -60,46 +55,9 @@ namespace NewsPortal.Controllers
         [Authorize(Roles = "Admin")]
         public ActionResult Delete(int id)
         {
-            ChangeLanguage();
             var articleId = service.GetArticleIdByCommentId(id);
             service.DeleteComment(id);
             return RedirectToRoute(new { controller = "Admin", action = "Details", id = articleId.ToString() });
-        }
-
-        public ActionResult ChangeCulture(string language)
-        {
-            List<string> cultures = new List<string>() { "ru", "en" };
-            if (!cultures.Contains(language))
-            {
-                language = "ru";
-            }
-            HttpCookie cookie = Request.Cookies["lang"];
-            if (cookie != null)
-            {
-                cookie.Value = language;
-            }
-            else
-            {
-                cookie = new HttpCookie("lang");
-                cookie.HttpOnly = false;
-                cookie.Value = language;
-                cookie.Expires = DateTime.Now.AddYears(1);
-            }
-            Response.Cookies.Add(cookie);
-            return RedirectToAction("", new { lang = language });
-        }
-
-        public void ChangeLanguage()
-        {
-            var currentLanguage = Request.RequestContext.RouteData.Values["lang"].ToString();
-            if (currentLanguage == "en")
-            {
-                ChangeCulture("en");
-            }
-            else
-            {
-                ChangeCulture("ru");
-            }
         }
     }
 }
