be7aaa4f41395c063d461a3adc29b8786b81f83b
Sasha <sanyokkotenko2002@gmail.com>
2020-08-18 11:40:06 +0300

    add NHBaseRepository, change paging icons(move to previous/next)

--- NHibernate.DAL/Repositories/CommentRepository.cs
+++ NHibernate.DAL/Repositories/CommentRepository.cs
@@ -1,53 +1,16 @@
 ﻿using Business.Interfaces;
 using Business.Models;
-using Business.Services;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace NHibernate.DAL.Repositories
 {
-    public class CommentRepository : ICommentRepository
+    public class CommentRepository : NHBaseRepository<Comment>, ICommentRepository
     {
-        private ISession Session
-        {
-            get
-            {
-                return NHibernateHelper.GetSession();
-            }
-        }
-
-        public IEnumerable<Comment> GetAll()
-        {
-            var comments = Session.Query<Comment>();
-            return comments;
-        }
-
-        public Comment Get(int id)
-        {
-            var comment = Session.Get<Comment>(id);
-            return comment;
-        }
-
         public IEnumerable<Comment> GetCommentsBy(int articleId)
         {
             var comments = Session.Query<Comment>().Where(c => c.ArticleId == articleId).ToList();
             return comments;
-        }
-        
-        public void Create(Comment comment)
-        {
-            Session.Save(comment);  
-        }
-
-        public void Update(Comment comment)
-        {
-            Session.Update(comment);
-        }
-
-        public void Delete(int id)
-        {
-            var comment = Session.Get<Comment>(id);
-            Session.Delete(comment);
         }
     }
 }
---
+++ NHibernate.DAL/Repositories/NHBaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using Business.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace NHibernate.DAL.Repositories
+{
+    public class NHBaseRepository<T> : IRepository<T> where T : class, IEntity
+    {
+        protected ISession Session
+        {
+            get
+            {
+                return NHibernateHelper.GetSession();
+            }
+        }
+
+        public void Create(T item)
+        {
+            Session.Save(item);
+        }
+
+        public virtual void Delete(int id)
+        {
+            var item = Session.Get<T>(id);
+            Session.Delete(item);
+        }
+
+        public T Get(int id)
+        {
+            var item = Session.Get<T>(id);
+            return item;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            var items = Session.Query<T>();
+            return items;
+        }
+
+        public virtual void Update(T item)
+        {
+            Session.Update(item);
+        }
+    }
+}
